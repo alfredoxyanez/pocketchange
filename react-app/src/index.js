19d89f6d20476ab726b7a4772ea8cd6d1d8ee3c8
ayanez17 <fredyyanez39@yahoo.com>
2017-12-15 20:43:38 -0500

    added middleware

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {
+  Provider
+} from 'react-redux';
+import {
+  createStore,
+  applyMiddleware
+} from 'redux';
+import allReducers from './reducers/allReducers';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(allReducers, applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
